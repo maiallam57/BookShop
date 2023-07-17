e5af0e1ecf671a2fb25770c79cdfcdd3ff424d9c
Mai allam <maimallam57@gmail.com>
2023-07-17 15:55:20 +0300

    Footer Edits

--- BulkyBookShopWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookShopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -66,6 +66,12 @@ namespace BulkyBookShopWeb.Controllers
             return View();
         }
 
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
